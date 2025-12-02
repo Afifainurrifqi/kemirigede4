3f84862919ac030b9cc378152d59d21200865796
Afifainurrifqi <afifainurrifqi@gmail.com>
2025-12-03 06:05:23 +0700

    bismillah

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -58,7 +58,7 @@
             <div class="row">
                 <div class="double-items text-light">
                     <div class="info col-md-7">
-                        <img src="/assets3/img/logosi.png" class="wow slideInLeft" style="width: 30%">
+                        <img src="/assets3/img/logosi3.png" class="wow slideInLeft" style="width: 30%">
                         <h2 class="wow slideInLeft">SITAKRO Kemirigede </h2>
                         <p class="wow fadeInLeft">
                             Sistem Informasi Data Mikro yang dimiliki oleh Desa Kemirigede Kecamatan Kemirigede Kabupaten
