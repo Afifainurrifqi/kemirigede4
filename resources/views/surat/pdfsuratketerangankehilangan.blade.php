7cbf608f9d3ad853ea60c83daed967404308864e
Afifainurrifqi <afifainurrifqi@gmail.com>
2026-07-01 18:44:44 +0700

    bismillah

--- resources/views/surat/pdfsuratketerangankehilangan.blade.php
+++ resources/views/surat/pdfsuratketerangankehilangan.blade.php
@@ -183,7 +183,7 @@
 
                 <td class="kop-text">
                     <div class="kop-baris-1">PEMERINTAH KABUPATEN BLITAR</div>
-                    <div class="kop-baris-2">KECAMATAN KEMIRIGEDE</div>
+                    <div class="kop-baris-2">KECAMATAN KESAMBEN</div>
                     <div class="kop-baris-3">PEMERINTAH DESA KEMIRIGEDE</div>
                     <div class="kop-alamat">Jln. Merdeka No. 74 Telp. 082139324445</div>
                     <div class="kop-kontak">
@@ -283,7 +283,7 @@
                 <img src="{{ public_path('assets/images/ttd.png') }}" class="ttd-img" alt="Tanda Tangan">
             </div> --}}
 
-            <br>
+            <br><br>
 
             <p><strong><u>MOH. HAMID ALMAULUDI S.Pd.I</u></strong></p>
 
