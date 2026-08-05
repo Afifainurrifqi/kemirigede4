c50ea784a45581f85c7d721bd4af7a84948d80e2
Afifainurrifqi <afifainurrifqi@gmail.com>
2026-08-05 21:19:48 +0600

    bismillah

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -531,7 +531,7 @@
                 alt="Logo SITAKRO"
             >
             <span class="brand-fallback" id="brandFallback" aria-hidden="true">S</span>
-            <span>SITAKRO WATES</span>
+            <span>KEMIRIGEDE</span>
         </div>
 
         <div class="lock-scene" aria-hidden="true">
